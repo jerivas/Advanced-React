3bb353787f54ff652f7a680da849e97e468064b8
Ed Rivas <ed@jerivas.com>
2021-05-21 17:36:22 -0600

    Add Header and Nav

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Header() {
+    return <header>
+        <div className="bar">
+            <Link href="/">Sick Fits</Link>
+        </div>
+        <div className="sub-bar">
+            <p>Search</p>
+        </div>
+    </header>
+}
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,10 @@
+import Header from './Header'
+import Nav from './Nav'
+
 export default function Page({ children }) {
     return <div>
-        <h1>THE PAGE</h1>
+        <Header />
+        <Nav />
         {children}
     </div>
 }
