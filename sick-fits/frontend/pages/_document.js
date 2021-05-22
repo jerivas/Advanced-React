46ac6fa709599ba62f86a76c5c3acc68da8f1142
Ed Rivas <ed@jerivas.com>
2021-05-22 08:24:40 -0600

    Fix styled components 🤷‍♂️
    https://github.com/vercel/next.js/tree/canary/examples/with-styled-components

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,14 +1,32 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+/* eslint-disable react/jsx-props-no-spreading */
+
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en-US">
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
