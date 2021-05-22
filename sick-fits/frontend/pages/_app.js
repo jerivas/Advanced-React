12d90fabe8701fa5de82710fc7abd44862b16797
Ed Rivas <ed@jerivas.com>
2021-05-22 15:59:37 -0600

    Add progress bar on route change

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading */
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
