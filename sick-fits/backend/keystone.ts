43b250d9613b926d16acac8e33c2d15c28a8f84a
Ed Rivas <ed@jerivas.com>
2021-05-25 22:13:50 -0600

    Set up keystone

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,25 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseUrl = process.env.DATABASE_URL
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360,
+    secret: process.env.COOKIE_SECRET
+}
+
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true
+        }
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseUrl
+    },
+    lists: createSchema({}),
+    ui: {
+        isAccessAllowed: () => true
+    }
+})
