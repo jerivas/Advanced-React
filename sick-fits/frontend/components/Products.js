ce60d3ea935f2fed3fdd4ed1a0098fd9b6c385d5
Ed Rivas <ed@jerivas.com>
2021-05-28 16:48:28 -0600

    Add actual pagination

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -2,10 +2,11 @@ import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Product from './Product';
+import { perPage } from '../config';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($first: Int!, $skip: Int!) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       description
@@ -27,14 +28,14 @@ const ProductListStyles = styled.div`
   grid-gap: 60px;
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: { skip: perPage * (page - 1), first: perPage },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       <ProductListStyles>
