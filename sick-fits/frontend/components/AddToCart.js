8ac1b4d2a8d12ec143a23eb8e89b819592bf814c
Ed Rivas <ed@jerivas.com>
2021-06-02 11:25:00 -0600

    Add AddToCart button

---
+++ sick-fits/frontend/components/AddToCart.js
@@ -0,0 +1,22 @@
+import { gql, useMutation } from '@apollo/client';
+import { CURRENT_USER_QUERY } from './User';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart(productId: $id) {
+      id
+    }
+  }
+`;
+
+export default function AddToCart({ id }) {
+  const [add, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
+    variables: { id },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  return (
+    <button type="button" onClick={add} disabled={loading}>
+      Add{loading && 'ing'} to Cart 🛒
+    </button>
+  );
+}
